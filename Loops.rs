078a0579cb1559a558b0d65c8ec00c04554b5a36
Andrey <93141508+svpra@users.noreply.github.com>
2022-02-10 14:26:02 +0300

    Create Loops.rs

---
+++ Loops.rs
@@ -0,0 +1,16 @@
+// --snip--
+
+    println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+
+        // --snip--
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
+    }
+}
