c74ea354fbbafdb9a7b6730c5e2263792c61d718
Andrey <93141508+svpra@users.noreply.github.com>
2022-02-12 21:03:48 +0300

    Create heightweidthththththhtbuteasier.rs

---
+++ heightweidthththththhtbuteasier.rs
@@ -0,0 +1,9 @@
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
