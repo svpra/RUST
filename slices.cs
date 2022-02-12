756de23ab22000497b9af885c4dc6860bb58e6d3
Andrey <93141508+svpra@users.noreply.github.com>
2022-02-12 19:26:51 +0300

    Create slices.cs

---
+++ slices.cs
@@ -0,0 +1,13 @@
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+fn main() {}
