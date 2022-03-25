56295a41e0ae0eb9f7d1884ea22c78d69da0f0a8
Andrey <93141508+svpra@users.noreply.github.com>
2022-03-25 12:21:40 +0300

    Create Vec.rs

---
+++ Vec.rs
@@ -0,0 +1,19 @@
+let mut vec = Vec::new();
+vec.push(1);
+vec.push(2);
+
+assert_eq!(vec.len(), 2);
+assert_eq!(vec[0], 1);
+
+assert_eq!(vec.pop(), Some(2));
+assert_eq!(vec.len(), 1);
+
+vec[0] = 7;
+assert_eq!(vec[0], 7);
+
+vec.extend([1, 2, 3].iter().copied());
+
+for x in &vec {
+    println!("{}", x);
+}
+assert_eq!(vec, [7, 1, 2, 3]);
